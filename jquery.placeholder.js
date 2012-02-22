241d27362e57361150c503b0e0b40ab8df093b45
David Murdoch <musicisair@yahoo.com>
2012-02-22 14:53:46 -0500

    Return an empty string for `input.value` in IE8-9 when an input's placeholder is active. Also, remove the `placeholder` class when the element's value is changed via JS (and not via $.fn.val)

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -6,7 +6,8 @@
 	    prototype = $.fn,
 	    valHooks = $.valHooks,
 	    hooks,
-	    placeholder;
+	    placeholder,
+	    Placeholder;
 
 	if (isInputSupported && isTextareaSupported) {
 
@@ -17,7 +18,7 @@
 		placeholder.input = placeholder.textarea = true;
 
 	} else {
-
+		
 		placeholder = prototype.placeholder = function() {
 			return this
 				.filter((isInputSupported ? 'textarea' : ':input') + '[placeholder]')
@@ -32,33 +33,100 @@
 
 		placeholder.input = isInputSupported;
 		placeholder.textarea = isTextareaSupported;
-
-		hooks = {
-			'get': function(element) {
-				var $element = $(element);
-				return $element.data('placeholder-enabled') && $element.hasClass('placeholder') ? '' : element.value;
-			},
-			'set': function(element, value) {
-				var $element = $(element);
-				if (!$element.data('placeholder-enabled')) {
-					return element.value = value;
-				}
-				if (value == '') {
-					element.value = value;
-					// We can’t use `triggerHandler` here because of dummy text/password inputs :(
-					setPlaceholder.call(element);
-				} else if ($element.hasClass('placeholder')) {
-					clearPlaceholder.call(element, true, value) || (element.value = value);
-				} else {
-					element.value = value;
-				}
-				// `set` can not return `undefined`; see http://jsapi.info/jquery/1.7.1/val#L2363
-				return $element;
+		
+		var hasDefineProperty = false;
+		try{
+			if(Object.defineProperty && Object.defineProperty({},'x',{'get': function(){return true}}).x){
+				hasDefineProperty = true;
 			}
-		};
-
-		isInputSupported || (valHooks.input = hooks);
-		isTextareaSupported || (valHooks.textarea = hooks);
+		}
+		catch(ლ_ಠ益ಠ_ლ){ /*do nothing*/ }
+		
+		// test for Object.defineProperty
+		if ( hasDefineProperty ) {
+			
+			// We use a string wrapper so we can detect if the value
+			// of our placeholder was changed.
+			Placeholder = function (value){ this.value = value; };
+			Placeholder.prototype = {
+				'toString': function(){
+					return this.value;
+				},
+				'valueOf': function(){
+					return this.value;
+				}
+			};
+			
+			// TODO: figure out how to define getters and setters for innerHTML on the HTMLTextAreaElement
+			// Monkey patch input and textareas so that when we get their values we can opt to return ""
+			// if our placeholder is active. 
+			$.each( [HTMLInputElement, HTMLTextAreaElement], function(i, obj){
+				var prototype = obj.prototype,
+					prop = "value",
+					originalValuePropDesc = Object.getOwnPropertyDescriptor(prototype, prop),
+					set = originalValuePropDesc.set,
+					get = originalValuePropDesc.get,
+					hooks = {
+						"get": function () {
+							var lastValue = $(this).data("placeholder-last-" + prop),
+								browserValue = get.call(this);
+							// if our value is a Placeholder and its value is equal to the currentValue
+							// we really are an "empty" string.
+							return lastValue instanceof Placeholder && browserValue == lastValue ? "" : browserValue;
+						},
+						"set": function (value) {
+							var $element = $(this);
+							// trigger the browser's' default `set`
+							set.call(this, value);
+							
+							// If we are not an instance of Placeholder then we
+							// were not set by this plugin so remove our placeholder class
+							if(!(value instanceof Placeholder)){
+								$element.removeClass("placeholder");
+							}
+							
+							// originalValuePropDesc.set coerces our Placeholder object to a string
+							// which is not what we want.
+							$element.data("placeholder-last-" + prop, value);
+							
+						}
+					};
+				Object.defineProperty(prototype, prop, hooks);
+			});
+			
+		}
+		else{
+			
+			Placeholder = String;
+			
+			hooks = {
+				'get': function(element) {
+					var $element = $(element);
+					return $element.data('placeholder-enabled') && $element.hasClass('placeholder') ? '' : element.value;
+				},
+				'set': function(element, value) {
+					var $element = $(element);
+					if (!$element.data('placeholder-enabled')) {
+						return element.value = value;
+					}
+					if (value == '') {
+						element.value = value;
+						// We can’t use `triggerHandler` here because of dummy text/password inputs :(
+						setPlaceholder.call(element);
+					} else if ($element.hasClass('placeholder')) {
+						clearPlaceholder.call(element, true, value) || (element.value = value);
+					} else {
+						element.value = value;
+					}
+					// `set` can not return `undefined`; see http://jsapi.info/jquery/1.7.1/val#L2363
+					return $element;
+				}
+			};
+	
+			isInputSupported || (valHooks.input = hooks);
+			isTextareaSupported || (valHooks.textarea = hooks);
+			
+		}
 
 		$(function() {
 			// Look for forms
@@ -95,7 +163,9 @@
 	function clearPlaceholder(event, value) {
 		var input = this,
 		    $input = $(input);
-		if (input.value == $input.attr('placeholder') && $input.hasClass('placeholder')) {
+	    // input.value lies to us now in IE8,
+	    // it may == "" even if it's actual value is its placeholders value.
+		if ( (input.value == '' || input.value == $input.attr('placeholder')) && $input.hasClass('placeholder')) {
 			if ($input.data('placeholder-password')) {
 				$input = $input.hide().next().show().attr('id', $input.removeAttr('id').data('placeholder-id'));
 				// If `clearPlaceholder` was called from `$.valHooks.input.set`
@@ -142,7 +212,7 @@
 				// Note: `$input[0] != input` now!
 			}
 			$input.addClass('placeholder');
-			$input[0].value = $input.attr('placeholder');
+			$input[0].value = new Placeholder($input.attr('placeholder'));
 		} else {
 			$input.removeClass('placeholder');
 		}
